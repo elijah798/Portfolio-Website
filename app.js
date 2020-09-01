359e2afc4ce213833e566bcd6eb0bbb458b115a3
elijah798 <46266593+elijah798@users.noreply.github.com>
2020-08-31 20:06:05 -0700

    added text content and projects to index
    added text content to all pages and added projects to main page

--- app.js
+++ app.js
@@ -3,33 +3,55 @@ const app = express()
 const port = 3000
 const { data } = require('./data/projectData.json');
 const { Projects } = data;
+const projectLength = Projects.length;
 
+//set up for middleware
 app.set('view engine', 'pug')
 app.set('views', './views')
 app.use('/static', express.static('public'))
 
-app.get('/',(req, res) => {
-    res.render('index');
-})
+//sets route for index page
+app.get('/', (req, res) => {
+        res.render('index', {
+            Projectss: Projects,
+        });
+    })
+    //route to send client to about page
+app.get('/about', (req, res) => {
+        res.render('about');
+    })
+    //route that send data about project to project pages
+app.get('/project/:id', (req, res) => {
+        if (req.params.id <= projectLength - 1) {
+            res.render('project', {
+                projectName: Projects[req.params.id].ProjectName,
+                description: Projects[req.params.id].description,
+                liveLink: Projects[req.params.id].live_link,
+                githubLink: Projects[req.params.id].github_link,
+                imageURL: Projects[req.params.id].image_urls,
+                tech: Projects[req.params.id].technologies,
+                imageOne: Projects[req.params.id].imagesOne,
+                imageTwo: Projects[req.params.id].imagesTwo,
+                imageThree: Projects[req.params.id].imagesThree,
+            });
+        } else {
+            //error route handling
+            res.status(404).send({
+                status: 404,
+                error: 'Not Found'
 
-app.get('/about',(req, res) => {
-    res.render('about');
-})
+            })
+        }
+    })
+    //error route handling
+app.use((req, res, next) => {
+        res.status(404).send({
+            status: 404,
+            error: 'Not Found'
 
-app.get('/project/:id',(req, res) => {
-    res.render('project', {
-        projectName: Projects[req.params.id].Project,
-        description: Projects[req.params.id].description,
-        liveLink: Projects[req.params.id].live_link,
-        githubLink: Projects[req.params.id].github_link,
-        imageURL: Projects[req.params.id].image_urls,
-        tech: Projects[req.params.id].technologies,
-        imageOne: Projects[req.params.id].imagesOne,
-        imageTwo: Projects[req.params.id].imagesTwo,
-        imageThree: Projects[req.params.id].imagesThree,
-    });
-})
-
+        })
+    })
+    //start express 
 app.listen(port, () => {
     console.log(`app listening on localhost:${port}`)
 })
