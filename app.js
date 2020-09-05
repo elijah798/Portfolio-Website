c599243f4c829e1e84704e19996f77a27fd68574
elijah798 <46266593+elijah798@users.noreply.github.com>
2020-09-05 16:39:29 -0700

    fixed naming issue

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 3000
-const { data } = require('./data/data.json');
+const { data } = require('./data/Data.json');
 const { Projects } = data;
 const projectLength = Projects.length;
 
